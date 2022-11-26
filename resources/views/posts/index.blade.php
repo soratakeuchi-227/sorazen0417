170933520f4186481cda1c4c59c71d20984085c1
soratakeuchi-227 <soratakeuchi0417@gmail.com>
2022-11-27 02:01:33 +0900

    ブログ投稿の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,16 +10,29 @@
        
     </head>
     <body class="antialiased">
-       <h1>Blog Name</h1>
-       <a href="/posts/create">create</a>
+       <h1>Blog Name</h1>>create</a>
        <div class="posts">
            @foreach ($posts as $post)
            <div class="post">
                <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
                <p class='body'>{{ $post->body }}</p>
+               <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                   @csrf
+                   @method('DELETE')
+        　　　 <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+        　　　 </form>
            </div>
            @endforeach
         </div>
         <div class='paginate'>{{ $posts->links() }}</div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById(`form_${id}`).submit();
+                }
+          }
+        </script>
     </body>
 </html>
